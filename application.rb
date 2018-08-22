b6ab1aff66f0a54cd8140802af9571ad09362e6e
oleksandrkits <oleksandrkit@tahometer.com>
2018-08-22 18:00:20 +0300

    #3 Small clean code fixes.

--- application.rb
+++ application.rb
@@ -2,7 +2,9 @@ require_relative 'product'
 require_relative 'vendor'
 
 # Creating new product
-product = Product.new("Bread", 12)
+product_bread = Product.new('Bread', 12)
+product_milk = Product.new('Milk', 23)
 
 # Creating new vendor
-vendor = Vendor.new("Steave",[product])
+product_list = [product_bread, product_milk]
+vendor = Vendor.new('Steve',product_list)
