84ac34e12add593521a8ed6e4fd7bba7347b2eac
oleksandrkits <oleksandrkit@tahometer.com>
2018-08-23 12:17:16 +0300

    #4 Implicit vendors class.

--- application.rb
+++ application.rb
@@ -2,9 +2,29 @@ require_relative 'product'
 require_relative 'vendor'
 
 # Creating new product
-product_bread = Product.new('Bread', 12)
-product_milk = Product.new('Milk', 23)
+bread = Product.new('Bread', 121)
+milk = Product.new('Milk', 23)
+meat = Product.new('Meat', 87)
+cheese = Product.new('Cheese', 11)
+cola = Product.new('Cola', 62)
+candy = Product.new('Candy', 73)
+butter = Product.new('Butter', 112)
+tomato = Product.new('Tomato', 43)
+onion = Product.new('Onion', 12)
+fish = Product.new('Fish', 83)
+sauce = Product.new('Sauce', 121)
+carrot = Product.new('Carrot', 33)
 
 # Creating new vendor
-products = [product_bread, product_milk]
-vendor = Vendor.new('Steve',products)
+products = [bread, milk, meat, cheese, cola, candy, butter, tomato, onion, fish, sauce, carrot]
+vendor = Vendor.new('Steve', products)
+
+# All products list
+puts vendor.products
+puts
+
+# All products cheaper than 100
+puts vendor.all_products_chp_than(100)
+
+# All pairs cheaper than 100
+vendor.show_all_pairs_chp_than(100)
--- vendor.rb
+++ vendor.rb
@@ -6,4 +6,33 @@ class Vendor
     @name = name
     @products = products
   end
+
+  def all_products_chp_than(val)
+    products.select {|pr| pr.price < val}
+  end
+
+  # returns all product pairs which price sum is cheaper than val
+  def all_pairs_chp_than(val)
+    pairs_products = []
+    (0...products.length).each do |i|
+      if products[i].price < val
+        (i+1...products.length).each do |j|
+          if products[j].price + products[i].price < val
+            pairs_products << [products[j], products[i]]
+          end
+        end
+      end
+    end
+    return pairs_products
+  end
+
+  def show_all_pairs_chp_than(val)
+    pairs = all_pairs_chp_than(val)
+    (0...pairs.length).each do |i|
+      puts
+      puts "#{pairs[i][0]}; #{pairs[i][1]}"
+    end
+  end
+
+  private :all_pairs_chp_than
 end
